48d53e2eef27b8b12d7906249b497e39e850b0ef
minilabmemo <minilabmemo@gmail.com>
2023-11-07 17:05:05 +0800

    Fix: 調整導覽列語意化

--- src/layouts/header.tsx
+++ src/layouts/header.tsx
@@ -17,13 +17,13 @@ export const Header = () => {
     <div className='header  p-tb-12 p-lr-48'>
       <div className='head  p-tb-12 p-lr-48'>
         <div className="logo-block"><img src={brand} alt="brand" /></div>
-        <div className='nav-block'>
-          <a className="nav link m-lr-8 p-8" href="#policy"> 候選人主張</a>
-          <a className="nav link m-lr-8 p-8" href="#policy">最新活動</a>
-          <a className="nav link m-lr-8 p-8" href="#policy">政策議題</a>
-          <a className="nav link m-lr-8 p-8" href="#policy">小額捐款</a>
-          <a className="nav link m-lr-8 p-8" href="#policy">民眾服務信箱</a>
-        </div>
+        <ul className='nav-block'>
+          <li className="nav m-lr-8 p-8"><a className="link" href="#policy"> 候選人主張</a></li>
+          <li className="nav m-lr-8 p-8"><a className="link" href="#policy">最新活動</a></li>
+          <li className="nav m-lr-8 p-8"><a className="link" href="#policy">政策議題</a></li>
+          <li className="nav m-lr-8 p-8"><a className="link" href="#policy">小額捐款</a></li>
+          <li className="nav m-lr-8 p-8"><a className="link" href="#policy">民眾服務信箱</a></li>
+        </ul>
         <div className='social-block'>
           <img src={icon_fb} alt="icon_fb" className="social-icon" />
           <img src={icon_ig} alt="icon_ig" className="social-icon" />
