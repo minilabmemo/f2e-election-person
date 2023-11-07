285114591d643d06f6fba9a810a3ab7ec1b5f71f
minilabmemo <minilabmemo@gmail.com>
2023-11-07 11:39:25 +0800

    Add: 調整樣式與檔案架構

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+
+import './styles/normalize.css';
+import './styles/fonts/inter.css';
+import './styles/style.scss';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
