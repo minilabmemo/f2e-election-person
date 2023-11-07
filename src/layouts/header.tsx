16b3058cb40f28cb46f3fe9255de700a093b2661
minilabmemo <minilabmemo@gmail.com>
2023-11-07 17:25:44 +0800

    Fix: 調整因為寬度不夠跑馬燈換行問題

--- src/layouts/header.tsx
+++ src/layouts/header.tsx
@@ -18,11 +18,11 @@ export const Header = () => {
       <div className='head  p-tb-12 p-lr-48'>
         <div className="logo-block"><img src={brand} alt="brand" /></div>
         <ul className='nav-block'>
-          <li className="nav m-lr-8 p-8"><a className="link" href="#policy"> 候選人主張</a></li>
-          <li className="nav m-lr-8 p-8"><a className="link" href="#policy">最新活動</a></li>
-          <li className="nav m-lr-8 p-8"><a className="link" href="#policy">政策議題</a></li>
-          <li className="nav m-lr-8 p-8"><a className="link" href="#policy">小額捐款</a></li>
-          <li className="nav m-lr-8 p-8"><a className="link" href="#policy">民眾服務信箱</a></li>
+          <li className="nav m-lr-8 p-8"><a href="#policy"> 候選人主張</a></li>
+          <li className="nav m-lr-8 p-8"><a href="#policy">最新活動</a></li>
+          <li className="nav m-lr-8 p-8"><a href="#policy">政策議題</a></li>
+          <li className="nav m-lr-8 p-8"><a href="#policy">小額捐款</a></li>
+          <li className="nav m-lr-8 p-8"><a href="#policy">民眾服務信箱</a></li>
         </ul>
         <div className='social-block'>
           <img src={icon_fb} alt="icon_fb" className="social-icon" />
