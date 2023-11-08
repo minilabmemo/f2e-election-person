87633d412447cf5c8e5f431bea9da744047c9a3f
minilabmemo <minilabmemo@gmail.com>
2023-11-08 16:40:40 +0800

    Fix: 調整主張區塊

--- src/layouts/content.tsx
+++ src/layouts/content.tsx
@@ -1,7 +1,7 @@
 
 import "./Content.scss";
 import banner_photo from "../assets/images/banner.png"
-import policy_1 from "../assets/images/policy_1.svg" //TODO
+import idea from "../assets/images/idea.png" //TODO
 
 import Articles from "../components/Articles";
 import p1_photo from "../assets/images/p1_photo.png"
@@ -26,7 +26,7 @@ export const Content = () => {
     <main>
 
       <div className="banner">
-        <div className="title">台灣的明天 喵先鋪路</div>
+        <div className="title-xl">台灣的明天 喵先鋪路</div>
         <div className="subtitle ">
           <div className="tag bg-primary m-r-16"> <div className="text">2024 立委參選人</div></div>
           <Person />
@@ -36,13 +36,27 @@ export const Content = () => {
       <div className="news-ticker">
         <div className="text">為喵星人，護台灣！從喵的眼中，看見台灣！喵的未來，人的希望</div>
       </div>
-      <section className="bg-orange p-y-104 " id="idea">
-        <div className="photo_policy">   <img src={policy_1} alt="policy_1" /></div>
+
+      <section className="bg-orange p-y-104" id="idea">
+        <div className="idea-wrap">
+          <div className="text-wrap">
+            <div className="title-group">
+              <h6 className="tag-small p-x-12 p-y-8 m-0">ADVOCATE</h6>
+              <div className="colorful-title"> 候選人主張</div>
+            </div>
+
+            <div className="subtitle">我堅信 ! 藉由推動更完善的<span className="text-orang-dark">貓咪福利</span>和相關政策，更是間接地投資於<span className="text-orang-dark">台灣的未來</span>。</div>
+            <div className="content">畢竟，民眾的身心健康與工作熱情是推動經濟的核心動力。透過完善的貓咪福利政策，為台灣的 GDP 經濟帶來巨大效益。
+              因此，我期望能在立法院內推進這些政策，確保每一隻貓咪都能得到他們應有的照顧，同時也為台灣的經濟發展助一臂之力。讓我們一同護航台灣的幸福經濟，從照顧每一隻貓咪開始。</div>
+          </div>
+          <div className="idea_photo">   <img src={idea} alt="idea_photo" /></div>
+
+        </div>
       </section>
 
       <section className="bg-white  p-y-104 " id="events">
-        <h6 className="tag-small p-x-12 p-y-8">   LATEST EVENTS</h6>
-        <div className="title"> 最新活動</div>
+        <h6 className="tag-small p-x-12 p-y-8">LATEST EVENTS</h6>
+        <div className="colorful-title"> 最新活動</div>
         <div className="m-t-64"></div>
         <Articles />
       </section>
@@ -50,7 +64,7 @@ export const Content = () => {
       <section className="bg-white  p-y-104 " id="policy">
 
         <h6 className="tag-small p-x-12 p-y-8"> POLICY ISSUES</h6>
-        <div className="title"> 政策議題</div>
+        <div className="colorful-title"> 政策議題</div>
         <div className="m-t-64"></div>
         <div className="policies">
           <div className="policy">
