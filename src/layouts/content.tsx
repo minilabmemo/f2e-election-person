52fd63140de8394829f7aff9389e18bb69e939a2
minilabmemo <minilabmemo@gmail.com>
2023-11-09 12:05:38 +0800

    Fix: policies width

--- src/layouts/content.tsx
+++ src/layouts/content.tsx
@@ -50,7 +50,7 @@ export const Content = () => {
             <div className="content">畢竟，民眾的身心健康與工作熱情是推動經濟的核心動力。透過完善的貓咪福利政策，為台灣的 GDP 經濟帶來巨大效益。
               因此，我期望能在立法院內推進這些政策，確保每一隻貓咪都能得到他們應有的照顧，同時也為台灣的經濟發展助一臂之力。讓我們一同護航台灣的幸福經濟，從照顧每一隻貓咪開始。</div>
           </div>
-          <div className="right">   <img src={idea} alt="idea_photo" /></div>
+          <div className="right"> <img src={idea} alt="idea_photo" /></div>
 
         </div>
       </section>
