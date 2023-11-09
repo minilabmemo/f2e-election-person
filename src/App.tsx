ecb1951c29c37e19c28614e08cca91115aad8466
minilabmemo <minilabmemo@gmail.com>
2023-11-09 15:06:38 +0800

    Add: 新增載入動畫

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,34 @@
 import { Content } from './layouts/Content';
 import { Footer } from './layouts/Footer';
 import { Header } from './layouts/Header';
+import gif from "./assets/gif/loading.gif"
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [isImageLoaded, setIsImageLoaded] = useState(false);
+
+  useEffect(() => {
+    const img = new Image();
+    img.src = gif;
+    img.onload = () => {
+      setIsImageLoaded(true);
+    };
+  }, []);
   return (
     <div className="App">
+
       <div className="base">
+        <div className="loading-wrap">
+          <div className="gif-wrap">  {isImageLoaded ? (
+            <img src={gif} alt="gif" />
+          ) : (
+            <p >Loading...</p>
+          )}</div>
+        </div>
         <div className="container">
           <Header></Header>
           <Content></Content>
           <Footer></Footer>
-
         </div>
       </div>
 
