42d89b0e14ccdaf9f561467cf7f8b8942d7c2632
minilabmemo <minilabmemo@gmail.com>
2023-11-15 12:31:16 +0800

    Fix: styles

--- src/components/modals/ItemsModal.tsx
+++ src/components/modals/ItemsModal.tsx
@@ -184,60 +184,7 @@ function ServicesModalStyle({ selectedItem, selectedIndex }: { selectedItem: Ser
 
 
 
-      {/* <div className="modal_main">
-        <div className="modal_main_photo">{selectedItem.imageNode}</div>
-        <h6 className="m-y-8">{selectedItem.title}</h6>
-        <div className="f-normal">
 
-          <div className='share-block'>
-            <div>分享</div>
-            <img src={icon_fb} alt="icon_fb" className="social-icon" />
-            <img src={icon_ig} alt="icon_ig" className="social-icon" />
-            <img src={icon_line} alt="icon_line" className="social-icon" />
-          </div>
-
-
-        </div>
-      </div>
-      <div className="modal_aside">
-        <h2 className="m-y-8 text-orang-dark">{selectedItem.title}</h2>
-
-        <div className="m-b-32"></div>
-        <div className="modal_item_content f-normal">{selectedItem.content}</div>
-
-        <div className="modal_more p-8">
-          <div className="f-w-600 m-b-16">
-            {selectedItem && 'policyProperty' in selectedItem ? "更多政策議題" : "更多活動"}
-
-
-          </div>
-
-          <div className="modal_more_contents">
-            {selectedItem && 'policyProperty' in selectedItem ? (
-              <> {policyItems
-                .filter((item, index) => index !== selectedIndex)
-                .slice(0, 2)
-                .map((item, index) => (
-                  <div className="modal_more_content" key={index + 1} >
-                    <div className="modal_more_photo">{item.imageNode}</div>
-                    <div className="m-y-8 f-normal">{item.title}</div>
-                  </div>
-                ))}</>
-            ) : (
-              <> {articleItems
-                .filter((item, index) => index !== selectedIndex)
-                .slice(0, 2)
-                .map((item, index) => (
-                  <div className="modal_more_content" key={index + 1} >
-                    <div className="modal_more_photo">{item.imageNode}</div>
-                    <div className="m-y-8 f-normal">{item.title}</div>
-                  </div>
-                ))}</>
-            )}
-
-          </div>
-        </div>
-      </div> */}
     </>
   )
 }
