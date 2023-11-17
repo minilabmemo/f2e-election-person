d3906fd082a5796415941dc8628b6762c9fe4cb9
minilabmemo <minilabmemo@gmail.com>
2023-11-17 11:38:48 +0800

    Fix: tidy styles

--- src/components/modals/ItemsModal.tsx
+++ src/components/modals/ItemsModal.tsx
@@ -174,7 +174,7 @@ function ServicesModalStyle({ selectedItem, selectedIndex }: { selectedItem: Ser
                 <h5 className="title">自訂贊助金額</h5>
                 <input type="text" className="service_input" placeholder="NT$ 輸入金額" />
               </div>
-              <div className="service_btn bg-orang-dark ">前往捐款</div>
+              <div className="service_btn bg-orang-dark f-w-600">前往捐款</div>
             </div>
           )
             :
@@ -188,7 +188,7 @@ function ServicesModalStyle({ selectedItem, selectedIndex }: { selectedItem: Ser
                 <input type="text" className="service_input" placeholder="手機" />
                 <h5 className="title">您的建言</h5>
                 <input type="text" className="service_input large" placeholder="輸入內容" />
-                <div className="service_btn bg-orang-dark ">送出意見</div>
+                <div className="service_btn bg-orang-dark f-w-600">送出意見</div>
               </div>
             )
           }
@@ -221,7 +221,7 @@ export const ItemsModal: React.FC<ItemsModalProps> = ({ isModalOpen, selectedIte
 
           </h2></div>
 
-          <div className="modal_content p-x-48 ">
+          <div className="modal_content p-x-48 p-b-32">
             {selectedItem && 'serviceProperty' in selectedItem ?
               (
 
